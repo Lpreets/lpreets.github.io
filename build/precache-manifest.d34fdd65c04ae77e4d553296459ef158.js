154a3fddc0de59c1abb8145aabf2db040b528437
Lovepreet Zelenkov Singh <113479651+Lpreets@users.noreply.github.com>
2023-06-24 20:16:07 +0200

    Whole Project

---
+++ build/precache-manifest.d34fdd65c04ae77e4d553296459ef158.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec53986c7faa7c78b2b0be6c8fbdef09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5521e0d8f22437f46cc",
+    "url": "/static/css/2.e2d6aeeb.chunk.css"
+  },
+  {
+    "revision": "eed38b2c16b5cad89adc",
+    "url": "/static/css/main.c113765c.chunk.css"
+  },
+  {
+    "revision": "c5521e0d8f22437f46cc",
+    "url": "/static/js/2.04fde22e.chunk.js"
+  },
+  {
+    "revision": "eed38b2c16b5cad89adc",
+    "url": "/static/js/main.df36b73f.chunk.js"
+  },
+  {
+    "revision": "aeaf5c606a64ce9d6b4c",
+    "url": "/static/js/runtime~main.ac5a3147.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
